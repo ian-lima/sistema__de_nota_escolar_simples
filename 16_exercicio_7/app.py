b3f9ca4040a1c60a4d266bc9cbf7d809f7f783eb
iandelimat <ianmarialima@gmail.com>
2021-12-17 18:34:01 -0300

    meu primeiro sistema de nota escolar simples

---
+++ 16_exercicio_7/app.py
@@ -0,0 +1,22 @@
+# Nota total de cada bimestre
+nota1 = 9
+nota2 = 7
+nota3 = 5
+
+media = 7
+
+print(nota1 >= media)
+print(nota2 >= media)
+print(nota3 >= media)
+
+
+# Notas das provas
+prova_a = 9.56
+prova_b = 7.88
+prova_c = 6.5
+
+media_provas = (prova_a + prova_b + prova_c) / 3
+
+print(media_provas)
+
+print(media_provas >= media)
